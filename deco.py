0f55cabec78386581b1b245f19a08ce222367460
bmohan048@gmail.com <bmohan048@gmail.com>
2023-05-09 10:58:59 +0530

    added super cool function

--- deco.py
+++ deco.py
@@ -1,13 +1,6 @@
-def hello(name="Jose"):
-    #scope of below hello as well as welcome is within hello function
-    print("The hello() function has been executed!!")
-    def greet():
-        return "\t This is a greet() func inside hello"
-    def welcome():
-        return "\t This is welcome() inside hello"
-    if name == 'Jose':
-        return greet
-    else:
-        return welcome 
-mynewfunc=hello("Jose")
-print(mynewfunc())
+def cool():
+    def super_cool():
+        return "I am very cool"
+    return super_cool
+some_func=cool()
+print(some_func())
